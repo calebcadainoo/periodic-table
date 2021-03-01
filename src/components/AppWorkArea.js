e0496d8ac069347e6753bbfc1e360f54e1d45d42
Caleb Crosy Adaioo <code.crosby@gmail.com>
2021-03-01 14:21:08 +0000

    succesfully listed all the chemical elements in UI after many tries and iterrations

--- src/components/AppWorkArea.js
+++ src/components/AppWorkArea.js
@@ -3,31 +3,23 @@ import './AppWorkArea.css'
 import pt from 'periodic-table'
 
 function AppWorkArea() {
-  let allElements = pt.all()
-  console.log(allElements)
-
-  let printElements = () => {
-    allElements.map((element) => {
-      return(<aside key={ element.atomicNumber } className="element">
-      <div className="element-atomic-no">{ element.atomicNumber }</div>
-      <div className="element-symbol">{ element.syombol }</div>
-      <div className="element-name">{ element.name }</div>
-    </aside>)
-    })
-  }
+  // let allElements = pt.all()
+  // console.log(pt.elements)
+  const data = pt.elements
+  // console.log(pt.elements)
 
   return (
     <div className="centroid workarea">
       <h1>Periodic Table</h1>
       <div className="workarea-elements">
-        {/* element */}
-        <aside className="element">
-          <div className="element-atomic-no">1</div>
-          <div className="element-symbol">H</div>
-          <div className="element-name">Hydrogen</div>
-        </aside>
-        {printElements()}
-        FEELING SLEEPY
+        {Object.entries(data).map(
+          (element) => (
+            <aside key={ element[1].atomicNumber } className="element">
+              <div className="element-atomic-no">{ element[1].atomicNumber }</div>
+              <div className="element-symbol">{ element[1].symbol }</div>
+              <div className="element-name">{ element[1].name }</div>
+            </aside>)
+        )}
       </div>
     </div>
   )
