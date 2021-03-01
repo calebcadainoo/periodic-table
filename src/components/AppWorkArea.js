1fdc81dbaad9fa35ab9a89e45fd80c9df2080166
Caleb Crosy Adaioo <code.crosby@gmail.com>
2021-03-01 14:57:52 +0000

    succesfully arranged the elements as it is in the periodic table in UI/component

--- src/components/AppWorkArea.js
+++ src/components/AppWorkArea.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import './AppWorkArea.css'
-import pt from 'periodic-table'
+import pt from '../PeriodTableData.json'
 
 function AppWorkArea() {
   // let allElements = pt.all()
   // console.log(pt.elements)
   const data = pt.elements
-  // console.log(pt.elements)
+  console.log(data)
 
   return (
     <div className="centroid workarea">
       <h1>Periodic Table</h1>
       <div className="workarea-elements">
-        {Object.entries(data).map(
-          (element) => (
-            console.log(element[1])
-          ))}
+        {/* <aside className="element">
+          <div className="element-atomic-no">1</div>
+          <div className="element-symbol">H</div>
+          <div className="element-name">Hydrogen</div>
+        </aside> */}
+
+        {data.map((element) =>(
+          <aside key={element.name} style={{
+              gridColumn: element.xpos,
+              gridRow: element.ypos
+            }} className="element">
+            <div className="element-atomic-no">{element.number}</div>
+            <div className="element-symbol">{element.symbol}</div>
+            <div className="element-name">{element.name}</div>
+          </aside>
+        ))}
       </div>
     </div>
   )
