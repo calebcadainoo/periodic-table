3077c0c78ab5a2955ef523857fd864e0a6461329
Caleb Crosy Adaioo <code.crosby@gmail.com>
2021-03-03 14:22:28 +0000

    passed colorMap to the DataLayer (React Context API)

--- src/components/PeriodicTable/PeriodicTable.js
+++ src/components/PeriodicTable/PeriodicTable.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import pt from '../../PeriodTableData.json'
 import './PeriodicTable.css'
+import { useDataLayerValue } from '../../DataLayer'
 
 function PeriodicTable() {
   const data = pt.elements
+  const [{colorMap}, dispatch] = useDataLayerValue()
 
-  const colorMap = {
-    "noble gas": "#4d42b8",
-    "alkaline earth metal":"#e45143",
-    "diatomic nonmetal":"#D81159",
-    "alkali metal":"#8F2D56",
-    "transition metal":"#56423e",
-    "post-transition metal":"#7f4800",
-    "polyatomic nonmetal": "#00A300",
-    actinide: "#7f69e7",
-    lanthanide:"#008f7a",
-    metalloid:"#1e445d",
-  }
+  // const colorMap = {
+  //   "noble gas": "#4d42b8",
+  //   "alkaline earth metal":"#e45143",
+  //   "diatomic nonmetal":"#D81159",
+  //   "alkali metal":"#8F2D56",
+  //   "transition metal":"#56423e",
+  //   "post-transition metal":"#7f4800",
+  //   "polyatomic nonmetal": "#00A300",
+  //   actinide: "#7f69e7",
+  //   lanthanide:"#008f7a",
+  //   metalloid:"#1e445d",
+  // }
   
   return (
     <div className="periodic-table">
