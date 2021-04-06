79bca274fbcf629c49332cb643220243bd33616b
Caleb Crosby Adainoo <calcronoo@gmail.com>
2021-04-06 00:10:06 +0000

    feat: highlight elements by category

--- src/components/PeriodicTable/PeriodicTableLegend.js
+++ src/components/PeriodicTable/PeriodicTableLegend.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-empty-pattern */
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { useDataLayerValue } from '../../context-api/DataLayer'
 import { actionTypes } from '../../context-api/reducer'
 
@@ -8,37 +7,52 @@ function PeriodicTableLegend() {
   const [{}, dispatch] = useDataLayerValue()
   const legendItem = useRef(null)
 
-  const handleCategoryClick = function (e) {
-		let targetEl = e.target
-    if (targetEl.parentElement.className !== "pt-legend-item") {
-      targetEl = targetEl.parentElement
-      console.log('If Part', targetEl)
-    } else {
-      targetEl = targetEl.parentElement
-      console.log('Else Part', targetEl)
-    }
+  // const [cateHighlight, setCateHighlight] = useState('pt-legend-selected')
+  const [cateStyles, setCateStyles] = useState('')
+  const handleCategoryClick = function (value) {
+		// let targetEl = e.target
+    // if (targetEl.parentElement.className !== "pt-legend-item") {
+    //   targetEl = targetEl.parentElement
+    //   console.log('If Part', targetEl)
+    // } else {
+    //   targetEl = targetEl.parentElement
+    //   console.log('Else Part', targetEl)
+    // }
 
-		const elcate = targetEl.dataset.category
-    if (typeof elcate === "string") {
+		// const elcate = targetEl.dataset.category
+    // if (typeof elcate === "string") {
       dispatch({
         type: actionTypes.SET_PERIODIC_EL_OPACITY,
-        periodicElOpacity: 0.5
+        periodicElOpacity: 0.2
       })
-    }
+    // }
 
-		console.log(elcate)
+		// console.log(elcate)
+    setCateStyles(value)
+		console.log(cateStyles)
+
 	}
 
   return (
     <aside className="pt-legend">
-      {Object.keys(colorMap).map((key) => (
-        <div onClick={handleCategoryClick}
-            key={key} 
+      <style dangerouslySetInnerHTML={{__html: `aside[data-category="${cateStyles}"]{
+        opacity: 1 !important;
+        background: grey;
+      }`}} />
+      <style dangerouslySetInnerHTML={{__html: `
+        div[data-category="${cateStyles}"]{
+          opacity: 1 !important;
+          background: grey;
+        }
+      `}} />
+      {Object.keys(colorMap).map((category, keyId) => (
+        <div onClick={() => handleCategoryClick(category)}
+            key={keyId} 
             ref={legendItem}
-            data-category={key} 
+            data-category={category} 
             className="pt-legend-item">
-          <div className="pt-legend-color"style={{ background: colorMap[key] }}></div>
-          <div className="pt-legend-name">{key}</div>
+          <div className="pt-legend-color"style={{ background: colorMap[category] }}></div>
+          <div className="pt-legend-name">{category}</div>
         </div>
       ))}
     </aside>
