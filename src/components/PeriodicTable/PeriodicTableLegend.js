08bd780e8cbe03b00274a11f663e5541a1d925bb
Caleb Crosby Adainoo <calcronoo@gmail.com>
2021-04-07 02:18:22 +0000

    refactor: add category legend highlighter

--- src/components/PeriodicTable/PeriodicTableLegend.js
+++ src/components/PeriodicTable/PeriodicTableLegend.js
@@ -8,35 +8,20 @@ function PeriodicTableLegend() {
   const [{}, dispatch] = useDataLayerValue()
   const legendItem = useRef(null)
 
-  // const [cateHighlight, setCateHighlight] = useState('pt-legend-selected')
   const [cateStyles, setCateStyles] = useState('')
   const handleCategoryClick = function (e, value) {
-		// let targetEl = e.target
-    // if (targetEl.parentElement.className !== "pt-legend-item") {
-    //   targetEl = targetEl.parentElement
-    //   console.log('If Part', targetEl)
-    // } else {
-    //   targetEl = targetEl.parentElement
-    //   console.log('Else Part', targetEl)
-    // }
-
-		// const elcate = targetEl.dataset.category
-    // if (typeof elcate === "string") {
-      dispatch({
-        type: actionTypes.SET_PERIODIC_EL_OPACITY,
-        periodicElOpacity: 0.1
-      })
-    // }
+    dispatch({
+      type: actionTypes.SET_PERIODIC_EL_OPACITY,
+      periodicElOpacity: 0.1
+    })
     
     const pop = `.pt-legend-item[data-category="${value}"]`
     const remover = document.querySelectorAll('.pt-legend-item')
     Object.entries(remover).map((ptItem) => {
       return ptItem[1].classList.remove('pt-legend-item-selected')
-      // return console.log(ptItem[1])
     })
     document.querySelector(pop).classList.add('pt-legend-item-selected')
-		console.log(typeof remover, remover)
-    setCateStyles(value)
+		setCateStyles(value)
 	}
 
   return (
